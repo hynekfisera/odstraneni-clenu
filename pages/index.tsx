010b714f8351215fa005ac0f0d9fbe6bf9eb3b67
Hynek Fišera <hynek.fisera@seznam.cz>
2022-11-30 19:31:25 +0100

    Style changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -48,60 +48,53 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Odstranění členů z textu</title>
-        <meta name="description" content="Nástroj na odstranění členů z textu" />
+        <title>Pomůcka pro výuku jazyků - odstranění členů z textu</title>
+        <meta name="description" content="Aplikace pro učitele jazyků, která umožňuje jednoduše vytvářet cvičení na procičování členů." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="px-4 lg:px-8 flex flex-col gap-4 pt-8">
         <section>
           <div className="mb-4">
-            <h1 className="text-lg font-semibold mb-2 text-gray-800">Odstranění členů z textu</h1>
+            <h1 className="text-lg font-semibold mb-2 text-gray-800">Pomůcka pro výuku jazyků - odstranění členů z textu</h1>
             <div className="text-sm text-gray-600">
               Tlačítko <i>vložit ze schránky</i> vloží obsah schránky do levého pole. Po jakékoliv změně levého pole se pravé pole automaticky vygeneruje podle aktuálního nastavení. Stane se tak i při jakékoliv změně nastavení. Tlačítko <i>zkopírovat do schránky</i> uloží obsah pravého pole.
-              <button
-                className="underline select-none"
-                onClick={() =>
-                  setInput(
-                    "If you're looking for random paragraphs, you've come to the right place. When a random word or a random sentence isn't quite enough, the next logical step is to find a random paragraph. We created the Random Paragraph Generator with you in mind. The process is quite simple. Choose the number of random paragraphs you'd like to see and click the button. Your chosen number of paragraphs will instantly appear."
-                  )
-                }
-              >
-                Vložit testovací odstavec
-              </button>
             </div>
           </div>
-          <div className="mb-2">
+          <div className="mb-4">
             <label htmlFor="language" className="text-sm font-medium text-gray-900 select-none">
               Jazyk textu:{" "}
             </label>
             <select name="language" id="language" className="text-sm outline-none border-2 rounded-lg p-1 border-transparent focus:border-indigo-500">
               <option value="en">Angličtina</option>
+              <option value="de" disabled>
+                Němčina (v přípravě)
+              </option>
             </select>
           </div>
-          <div className="flex flex-wrap gap-2 items-center mb-3">
+          <div className={`flex flex-col gap-2 mb-3 hover:bg-gray-50 border-2 py-3 px-5 rounded-xl cursor-pointer ${mode ? "border-indigo-400" : "border-gray-300"}`} onClick={() => setMode(true)}>
             <div>
               <input checked={mode} onChange={(e) => setMode(e.target.checked)} id="underscores" type="radio" name="mode" className="accent-indigo-500 outline-none" />
-              <label htmlFor="underscores" className="ml-2 text-sm font-medium text-gray-900 select-none">
+              <label htmlFor="underscores" className="ml-2 text-sm font-medium text-gray-900 select-none cursor-pointer">
                 Nahradit podtržítky {mode && `(${count})`}
               </label>
               {mode && <input type="range" name="count" id="count" value={count} onChange={(e) => setCount(+e.target.value)} min={1} max={8} step={1} className="outline-none accent-indigo-500 ml-1" />}
             </div>
-            <div className="text-sm font-normal ml-2">
-              I wrote <span className="text-green-500">the</span> phrase &quot;<span className="text-orange-500">An</span> apple <span className="text-blue-500">a</span> day keeps <span className="text-purple-500">the</span> doctor away&quot; on <span className="text-red-500">a</span> piece of
-              paper. =&gt; I wrote <span className="text-green-500">___</span> phrase &quot;<span className="text-orange-500">___</span> apple <span className="text-blue-500">___</span> day keeps <span className="text-purple-500">___</span> doctor away&quot; on{" "}
+            <div className="text-sm font-normal ml-0.5">
+              Příklad: I wrote <span className="text-green-500">the</span> phrase &quot;<span className="text-orange-500">An</span> apple <span className="text-blue-500">a</span> day keeps <span className="text-purple-500">the</span> doctor away&quot; on <span className="text-red-500">a</span> piece
+              of paper. =&gt; I wrote <span className="text-green-500">___</span> phrase &quot;<span className="text-orange-500">___</span> apple <span className="text-blue-500">___</span> day keeps <span className="text-purple-500">___</span> doctor away&quot; on{" "}
               <span className="text-red-500">___</span> piece of paper.
             </div>
           </div>
-          <div className="flex flex-wrap gap-2 items-center mb-3">
+          <div className={`flex flex-col gap-2 mb-3 hover:bg-gray-50 border-2 py-3 px-5 rounded-xl cursor-pointer ${mode ? "border-gray-300" : "border-indigo-400"}`} onClick={() => setMode(false)}>
             <div>
               <input checked={!mode} onChange={(e) => setMode(!e.target.checked)} id="nothing" type="radio" name="mode" className="accent-indigo-500 outline-none" />
-              <label htmlFor="nothing" className="ml-2 text-sm font-medium text-gray-900 select-none">
+              <label htmlFor="nothing" className="ml-2 text-sm font-medium text-gray-900 select-none cursor-pointer">
                 Odstranit úplně
               </label>
             </div>
-            <div className="text-sm font-normal ml-2">
-              I wrote <span className="text-green-500">the</span> phrase &quot;<span className="text-orange-500">An</span> apple <span className="text-blue-500">a</span> day keeps <span className="text-purple-500">the</span> doctor away&quot; on <span className="text-red-500">a</span> piece of
-              paper. =&gt; I wrote phrase &quot;Apple day keeps doctor away&quot; on piece of paper.
+            <div className="text-sm font-normal ml-0.5">
+              Příklad: I wrote <span className="text-green-500">the</span> phrase &quot;<span className="text-orange-500">An</span> apple <span className="text-blue-500">a</span> day keeps <span className="text-purple-500">the</span> doctor away&quot; on <span className="text-red-500">a</span> piece
+              of paper. =&gt; I wrote phrase &quot;Apple day keeps doctor away&quot; on piece of paper.
             </div>
           </div>
         </section>
@@ -110,6 +103,16 @@ export default function Home() {
             <section className="flex flex-wrap gap-2">
               <button className="btn btn-primary" onClick={async () => setInput(await navigator.clipboard.readText())}>
                 Vložit ze schránky <FontAwesomeIcon icon={faArrowTurnDown} />
+              </button>
+              <button
+                className="btn btn-secondary"
+                onClick={() =>
+                  setInput(
+                    "If you're looking for random paragraphs, you've come to the right place. When a random word or a random sentence isn't quite enough, the next logical step is to find a random paragraph. We created the Random Paragraph Generator with you in mind. The process is quite simple. Choose the number of random paragraphs you'd like to see and click the button. Your chosen number of paragraphs will instantly appear."
+                  )
+                }
+              >
+                Vložit testovací odstavec
               </button>
               <button className="btn btn-secondary" onClick={() => setInput("")}>
                 Vymazat pole
